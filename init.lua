91f7d86cdf1b0626c613469b6636de97bbdf5f85
1F616EMO <root@1f616emo.xyz>
2026-01-19 17:53:01 +0800

    Add stereo channels except for "Ride"

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ local S = minetest.get_translator("phonograph_album_helena")
 
 local MP = minetest.get_modpath("phonograph_album_helena")
 local function songpath(name)
-    return table.concat({MP, "phonographs", "phonograph_album_helena_" .. name .. ".ogg"}, DIR_DELIM)
+    return table.concat({ MP, "phonographs", "phonograph_album_helena_" .. name .. ".ogg" }, DIR_DELIM)
 end
 
 local album = phonograph.register_album("phonograph_album_helena:album_helena", {
@@ -22,101 +22,35 @@ local album = phonograph.register_album("phonograph_album_helena:album_helena", 
     license = phonograph.licenses.CCBYSA4,
 })
 
-album:register_song("chaos", {
-    title = "Chaos",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("chaos"),
-    spec = {},
-})
-
-album:register_song("moonscapes", {
-    title = "Moonscapes",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("moonscapes"),
-    spec = {},
-})
-
-album:register_song("serious_adventure", {
-    title = "Serious Adventure",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("serious_adventure"),
-    spec = {},
-})
-
-album:register_song("shadowed_serenade", {
-    title = "Shadowed Serenade",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("shadowed_serenade"),
-    spec = {},
-})
-
-album:register_song("starscapes", {
-    title = "Starscapes",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("starscapes"),
-    spec = {},
-})
-
-album:register_song("symphony_of_strings", {
-    title = "Symphony of Strings",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("symphony_of_strings"),
-    spec = {},
-})
-
-album:register_song("thundering_overture", {
-    title = "Thundering Overture",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("thundering_overture"),
-    spec = {},
-})
-
-album:register_song("thunderstruck", {
-    title = "Thunderstruck",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("thunderstruck"),
-    spec = {},
-})
-
-album:register_song("lofi", {
-    title = "LoFi",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("lofi"),
-    spec = {},
-})
-
-album:register_song("ride", {
-    title = "Ride",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("ride"),
-    spec = {},
-})
-
-album:register_song("neon_lights", {
-    title = "Neon Lights",
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("neon_lights"),
-    spec = {},
-})
+for _, data in ipairs({
+    { "chaos",               "Chaos",               nil },
+    { "moonscapes",          "Moonscapes",          nil },
+    { "serious_adventure",   "Serious Adventure",   nil },
+    { "shadowed_serenade",   "Shadowed Serenade",   nil },
+    { "starscapes",          "Starscapes",          nil },
+    { "symphony_of_strings", "Symphony of Strings", nil },
+    { "thundering_overture", "Thundering Overture", nil },
+    { "thunderstruck",       "Thunderstruck",       nil },
+    { "lofi",                "LoFi",                nil },
+    { "ride",                "Ride",                nil, false },
+    { "neon_lights",         "Neon Lights",         nil },
+}) do
+    local id, title, short_description, has_stereo = unpack(data)
+    album:register_song(id, {
+        title = title,
+        short_description = short_description,
+        long_description = nil,
+        artist = nil,
+        spec = {
+            filepath = songpath(id),
+        },
+        multichannel_specs = has_stereo and {
+            {
+                filepath = songpath(id .. "_ch0"),
+            },
+            {
+                filepath = songpath(id .. "_ch1"),
+            },
+        } or nil,
+    })
+end
